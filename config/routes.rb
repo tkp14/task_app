99906371388ae25dcc1e06c47b97e3d38c2666a2
tkp14 <tk.091438@gmail.com>
2021-05-22 22:29:40 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  root 'static_pages#home'
+  get :about,        to: 'static_pages#about'
+  get :help,        to: 'static_pages#help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
