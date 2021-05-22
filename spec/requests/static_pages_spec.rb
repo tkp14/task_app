e334aab35972f0e2a2ddae2a5c666632110bc981
tkp14 <tk.091438@gmail.com>
2021-05-22 22:55:03 +0900

    静的ページのリクエストテストkijyutu

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      get static_pages_path
+  describe "トップページ" do
+    it "正常なレスポンスを返すこと" do
+      get root_path
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "aboutページ" do
+    it "正常なレスポンスを返すこと" do
+      get about_path
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "helpページ" do
+    it "正常なレスポンスを返すこと" do
+      get help_path
+      expect(response).to be_success
       expect(response).to have_http_status(200)
     end
   end
