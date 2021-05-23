3370c6f53ab9637ce5107cdc7c2201a494cf1a48
tkp14 <tk.091438@gmail.com>
2021-05-23 16:43:49 +0900

    各ページのタイトルを動的に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')  # full_titleメソッドを定義
+    base_title = 'タスクシェア'
+    if page_title.blank?
+      base_title  # トップページはタイトル
+    else
+      "#{page_title} - #{base_title}" # トップ以外のページはタイトル「〇〇 - タスクシェア」
+    end
+  end
 end
