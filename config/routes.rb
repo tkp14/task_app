7408b2898e46e11c4996a463cd06f4281f54fa8a
tkp14 <tk.091438@gmail.com>
2021-05-24 22:59:06 +0900

    ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
-  get 'users/index'
   root 'static_pages#home'
-  get :about,        to: 'static_pages#about'
+  get :about,       to: 'static_pages#about'
   get :help,        to: 'static_pages#help'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get :signup,      to: 'users#new'
+  resources :users
 end
