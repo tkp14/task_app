d742bc9e996980a3b2ab6d06a1714e55cde85409
tkp14 <tk.091438@gmail.com>
2021-05-24 22:40:27 +0900

    usersコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
+  get 'users/index'
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :help,        to: 'static_pages#help'
