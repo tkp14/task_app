ac31035948eeff21b2d798486e0da08a5064957e
tkp14 <tk.091438@gmail.com>
2021-05-28 00:34:29 +0900

    モデルテストを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  context "バリデーション" do
+    it "メール、メールアドレスがあれば有効なこと" do
+      expect(user).to be_valid
+    end
+
+    it "名前がなければ無効なこと" do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
+
+    it "名前が50文字以内であること" do
+      user = build(:user, name: "a" * 51)
+      user.valid?
+      expect(user.errors[:name]).to include("は50文字以内で入力してください")
+    end
+
+    it "メールアドレスがなければ無効なこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "メールアドレスが255文字以内であること" do
+      user = build(:user, email: "#{"a" * 244}@example.com")
+      user.valid?
+      expect(user.errors[:email]).to include("は255文字以内で入力してください")
+    end
+
+    it "メールアドレスが不正の場合は無効なこと" do
+      user = build(:user, email: th@com)
+      user.valid?
+      expect(user.errors[:email]).to include("は不正な値です")
+    end
+
+    it "メールアドレスが重複している場合は無効なこと" do
+      other_user = build(:user, email: user.email)
+      other_user.valid?
+      expect(other_user.errors[:email]).to include("はすでに存在します")
+    end
+
+    it "メールアドレスは小文字で保存されること" do
+      email = "ExamPle@example.com"
+      user = create(:user, email: email)
+      expect(user.email).to eq email.downcase
+    end
+
+    it "パスワードがなければ無効な状態であること" do
+      user = build(:user, password: nil, password_confirmation: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "パスワードが6文字以上であること" do
+      user = build(:user, password: "a" * 6, password_confirmation: "a" * 6)
+      user.valid?
+      expect(user).to be_valid
+    end
+  end
 end
