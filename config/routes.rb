758014f5d4671e313798a95597fc8b929094bd93
tkp14 <tk.091438@gmail.com>
2021-05-30 23:47:23 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#home'
   get :about,       to: 'static_pages#about'
   get :help,        to: 'static_pages#help'
   get :signup,      to: 'users#new'
   resources :users
+  get :login,       to: 'sessions#new'
+  get :login,       to: 'sessions#create'
+  get :logout,       to: 'sessions#destroy'
 end
