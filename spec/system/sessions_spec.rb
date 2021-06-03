4bf352f349c6f048f465191372ecb9e54acb8b12
tkp14 <tk.091438@gmail.com>
2021-06-03 23:52:38 +0900

    システムテスト作成

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -36,12 +36,32 @@ RSpec.describe "Sessions", type: :system do
       end
     end
 
-    context "ログイン処理について" do
-      it "有効なユーザーの場合はログインできること" do
+    context "ログイン処理" do
+      it "無効なユーザーでログインを行うとログインが失敗することを確認" do
+        fill_in "user_email", with: "user@example.com"
+        fill_in "user_password", with: "pass"
+        click_button "ログイン"
+        expect(page).to have_content 'メールアドレスとパスワードの組み合わせが誤っています'
+
+        visit root_path
+        expect(page).not_to have_content "メールアドレスとパスワードの組み合わせが誤っています"
       end
 
-      it "無効なユーザーの場合はログインできないこと" do
+      it "有効なユーザーでログインする前後でヘッダーが正しく表示されていることを確認" do
+        expect(page).to have_link 'クックログとは？', href: about_path
+        expect(page).to have_link 'ユーザー登録(無料)', href: signup_path
+        expect(page).to have_link 'ログイン', href: login_path
+        expect(page).not_to have_link 'ログアウト', href: logout_path
 
+        fill_in "user_email", with: user.email
+        fill_in "user_password", with: user.password
+        click_button "ログイン"
+
+        expect(page).to have_link 'クックログとは？', href: about_path
+        expect(page).to have_link 'ユーザー一覧', href: users_path
+        expect(page).to have_link 'プロフィール', href: user_path(user)
+        expect(page).to have_link 'ログアウト', href: logout_path
+        expect(page).not_to have_link 'ログイン', href: login_path
       end
     end
   end
