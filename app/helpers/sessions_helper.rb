7cf47545a910d449c31face31fa0ee661dd3c3dd
tkp14 <tk.091438@gmail.com>
2021-06-05 23:50:51 +0900

    ログイン時、永続セッションを破棄する

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,11 +10,6 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
-
   # ユーザーのセッションを永続的にする
   def remember(user)
     user.remember
@@ -24,8 +19,6 @@ module SessionsHelper
 
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
@@ -36,3 +29,17 @@ module SessionsHelper
       end
     end
   end
+
+  # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,9 @@ class User < ApplicationRecord
       return false if remember_digest.nil?
       BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
+
+      # ユーザーのログイン情報を破棄する
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
 end
