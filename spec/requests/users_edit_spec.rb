2f97ca16821444556bf7113b98b398dd739e3420
tkp14 <tk.091438@gmail.com>
2021-06-13 00:00:05 +0900

    リクエストテスト追記

--- spec/requests/users_edit_spec.rb
+++ spec/requests/users_edit_spec.rb
@@ -2,29 +2,62 @@ require 'rails_helper'
 
 RSpec.describe "プロフィール編集", type: :request do
   let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
 
-  before do
-    get edit_user_path(user)
+  context "認可されたユーザーの場合" do
+    before do
+      login_for_request(user)
+      get edit_user_path(user)
+    end
+
+    it "正常なレスポンスを返すこと" do
+      expect(response).to be_success
+      expect(response).to render_template('users/edit')
+    end
+
+    it "有効なプロフィール更新の場合に正しく更新されること" do
+      patch user_path(user), params: { user: { name: "Example User",
+                                               email: "user@example.com",
+                                               introduction: "こんにちは" } }
+      redirect_to user
+      follow_redirect!
+      expect(response).to render_template('users/show')
+    end
+
+    it "無効なプロフィール更新の場合は更新できないこと" do
+      patch user_path(user), params: { user: { name: "",
+                                                email: "user@example.com",
+                                                introduction: "こんにちは" } }
+      expect(response).to render_template('users/edit')
+    end
   end
 
-  it "正常なレスポンスを返すこと" do
-    expect(response).to be_success
-    expect(response).to render_template('users/edit')
+  context "ログインしていないユーザーの場合" do
+    it "ログインページへリダイレクトされること" do
+      #編集
+      get edit_user_path(user)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to login_path
+      #更新
+      patch user_path(user), params: { user: { name: user.name,
+                                               email: user.email } }
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to login_path
+    end
   end
 
-  it "有効なプロフィール更新の場合" do
-    patch user_path(user), params: { user: { name: "Example User",
-                                             email: "user@example.com",
-                                             introduction: "こんにちは" } }
-    redirect_to user
-    follow_redirect!
-    expect(response).to render_template('users/show')
-  end
-
-  it "無効なプロフィール更新の場合" do
-    patch user_path(user), params: { user: { name: "",
-                                              email: "user@example.com",
-                                              introduction: "こんにちは" } }
-    expect(response).to render_template('users/edit')
+  context "認可されていないユーザーの場合" do
+    it "ホーム画面へリダイレクトされること" do
+      login_for_request(other_user)
+      #編集
+      get edit_user_path(user)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to root_path
+      #更新
+      patch user_path(user), params: { user: { name: user.name,
+                                               email: user.email } }
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to root_path
+    end
   end
 end
--- spec/requests/users_show_spec.rb
+++ spec/requests/users_show_spec.rb
@@ -3,9 +3,20 @@ require 'rails_helper'
 RSpec.describe "プロフィールページ", type: :request do
   let!(:user) { create(:user) }
 
-  it "正常なレスポンスを返すこと" do
-    get user_path(user)
-    expect(response).to be_success
-    expect(response).to render_template('users/show')
+  context "ログインしているユーザーの場合" do
+    it "正常なレスポンスを返すこと" do
+      login_for_request(user)
+      get user_path(user)
+      expect(response).to be_success
+      expect(response).to render_template('users/show')
+    end
+  end
+
+  context "ログインしていないユーザーの場合" do
+    it "ログインページへリダイレクトされること" do
+      get user_path(user)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to login_path
+    end
   end
 end
