cd8734ee8bd80e68ec993660f40ddff925d637cd
tkp14 <tk.091438@gmail.com>
2021-06-13 10:11:30 +0900

    フレンドリーフォワーディングのリクエストテストを記述

--- spec/requests/users_edit_spec.rb
+++ spec/requests/users_edit_spec.rb
@@ -5,17 +5,10 @@ RSpec.describe "プロフィール編集", type: :request do
   let!(:other_user) { create(:user) }
 
   context "認可されたユーザーの場合" do
-    before do
-      login_for_request(user)
+    it "有効なプロフィール更新の場合に正しく更新されること(+フレンドリーフォワーディング)" do
       get edit_user_path(user)
-    end
-
-    it "正常なレスポンスを返すこと" do
-      expect(response).to be_success
-      expect(response).to render_template('users/edit')
-    end
-
-    it "有効なプロフィール更新の場合に正しく更新されること" do
+      login_for_request(user)
+      expect(response).to redirect_to edit_user_url(user)
       patch user_path(user), params: { user: { name: "Example User",
                                                email: "user@example.com",
                                                introduction: "こんにちは" } }
@@ -25,6 +18,8 @@ RSpec.describe "プロフィール編集", type: :request do
     end
 
     it "無効なプロフィール更新の場合は更新できないこと" do
+      login_for_request(user)
+      get edit_user_path(user)
       patch user_path(user), params: { user: { name: "",
                                                 email: "user@example.com",
                                                 introduction: "こんにちは" } }
