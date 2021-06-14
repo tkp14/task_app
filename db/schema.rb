96b382b47260594b027fffb05d17b34f145c7b4c
tkp14 <tk.091438@gmail.com>
2021-06-14 16:16:14 +0900

    サンプルユーザーを100人作成

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2021_06_05_075016) do
     t.string "name"
     t.string "email"
     t.text "introduction"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
