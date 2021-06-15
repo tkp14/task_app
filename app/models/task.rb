5a858802ed234024224f677ae8ad579fc77e86f8
tkp14 <tk.091438@gmail.com>
2021-06-16 00:23:53 +0900

    taskモデルのバリデーション 追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :user_id, presence: true
+  validates :introduction, presence: true, length: { maximum: 50 }
 end
