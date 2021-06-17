dc07e92d3fdf3428b9a204ba6fcff20f6cf269da
tkp14 <tk.091438@gmail.com>
2021-06-17 23:56:54 +0900

    taskモデル修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
-    name "今日のタスク！"
-    introduction "10時から筋トレを30分間する"
+    name { Faker::Name.name }
+    introduction { "10時から筋トレを30分間する" }
     required_time { 30 }
   end
 end
