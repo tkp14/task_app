770d4e8cbdfd499146a6452149cad636ab7bdcac
tkp14 <tk.091438@gmail.com>
2021-06-17 23:58:21 +0900

    taskモデル、taskファクトリ修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,4 @@ class Task < ApplicationRecord
   validates  :user_id, presence: true
   validates  :name, presence: true, length: { maximum: 50 }
   validates  :introduction, presence: true, length: { maximum: 50 }
-  association :user
 end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { Faker::Name.name }
     introduction { "10時から筋トレを30分間する" }
     required_time { 30 }
+    association :user
   end
 end
