704f7646c54c6654c985967a51322be44f6a185d
tkp14 <tk.091438@gmail.com>
2021-06-18 00:22:49 +0900

    taskモデルに降順にする設定を記述

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates  :user_id, presence: true
   validates  :name, presence: true, length: { maximum: 50 }
   validates  :introduction, presence: true, length: { maximum: 50 }
