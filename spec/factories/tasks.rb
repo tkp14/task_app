47657d5640a3e1adcfd0fac760a2d000ff636108
tkp14 <tk.091438@gmail.com>
2021-06-18 00:23:36 +0900

    taskファクトリに作成日時の異なるtraitを追記

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -5,4 +5,16 @@ FactoryBot.define do
     required_time { 30 }
     association :user
   end
+
+  trait :yesterday do
+    created_at { 1.day.ago }
+  end
+
+  trait :one_week_ago do
+    created_at { 1.week.ago }
+  end
+
+  trait :one_month_ago do
+    created_at { 1.month.ago }
+  end
 end
