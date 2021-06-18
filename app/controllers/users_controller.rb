a0f1f0045c8858e3000a6bba0acaeb134cdb04ce
tkp14 <tk.091438@gmail.com>
2021-06-18 23:34:53 +0900

    Userコントローラのshowページでtaskを表示させる設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tasks = @user.tasks.paginate(page: params[:page], per_page: 5)
   end
 
   def new
