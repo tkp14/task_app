a39d59a6a12d2c0833dc6c90ec7e87e281024213
tkp14 <tk.091438@gmail.com>
2021-06-19 14:09:20 +0900

    seedsデータでタスクを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,9 @@ User.create!(name:  "山田 太郎",
               password:              password,
               password_confirmation: password)
 end
+
+10.times do |n|
+ Task.create!(name:  Faker::Name.name,
+              introduction: "今日の積み上げ",
+              user_id: 1)
+end
