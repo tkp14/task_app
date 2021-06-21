13e3b465d2c5ebdf1ff53277ac6a368be502b841
tkp14 <tk.091438@gmail.com>
2021-06-21 22:48:20 +0900

    タスクのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get :help,        to: 'static_pages#help'
   get :signup,      to: 'users#new'
   resources :users
+  resources :tasks
   get :login,       to: 'sessions#new'
   post :login,       to: 'sessions#create'
   delete :logout,       to: 'sessions#destroy'
