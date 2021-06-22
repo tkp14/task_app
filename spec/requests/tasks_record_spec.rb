1dd2e78eb8d774920aa8219bdba27e03ff0854bf
tkp14 <tk.091438@gmail.com>
2021-06-22 18:40:54 +0900

    リクエストテスト追記

--- spec/requests/tasks_record_spec.rb
+++ spec/requests/tasks_record_spec.rb
@@ -5,10 +5,30 @@ RSpec.describe "タスクについて", type: :request do
   let!(:task) { create(:task, user: user) }
 
   context "ログインしている場合" do
-    it "正常なレスポンスを返すこと" do
+    before do
       login_for_request(user)
       get new_task_path
+    end
+
+    it "正常なレスポンスを返すこと" do
       expect(response).to have_http_status(200)
+      expect(response).to render_template('tasks/new')
+    end
+
+    it "有効なデータの場合は投稿できること" do
+      expect {
+        post tasks_path, params: { task: { name: "今日の積み上げ",
+                                           introduction: "筋トレをする"} }
+      }.to change(Task, :count).by(1)
+      follow_redirect!
+      expect(response).to render_template('static_pages/home')
+    end
+
+    it "無効なデータの場合は投稿できないこと" do
+      expect {
+        post tasks_path, params: { task: { name: "",
+                                           introduction: ""} }
+      }.not_to change(Task, :count)
       expect(response).to render_template('tasks/new')
     end
   end
