eba21793fe149b19bf0e15262519ec4f24907515
tkp14 <tk.091438@gmail.com>
2021-06-22 17:54:21 +0900

    タスクの投稿の実装、createアクション追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,21 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクの投稿が完了しました！"
+      redirect_to root_url
+    else
+      render 'tasks/new'
+    end
+  end
+
+  private
+
+  #タスクの登録に必要な情報
+    def task_params
+      params.require(:task).permit(:name, :introduction)
+    end
 end
