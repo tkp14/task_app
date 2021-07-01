84c663ce7566d37af3993118574c771ac90feae5
tkp14 <tk.091438@gmail.com>
2021-07-01 23:26:58 +0900

    タスク登録のViewファイルにjQueryを追記

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,7 @@ class Task < ApplicationRecord
   validates  :user_id, presence: true
   validates  :name, presence: true, length: { maximum: 50 }
   validates  :introduction, presence: true, length: { maximum: 200 }
-  validate  :picture_size
+  validate   :picture_size
 
   private
 
