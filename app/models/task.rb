05e7fe816537376974376d95e8e42520ee15543b
tkp14 <tk.091438@gmail.com>
2021-07-01 23:08:11 +0900

    画像のリサイズ設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,14 @@ class Task < ApplicationRecord
   validates  :user_id, presence: true
   validates  :name, presence: true, length: { maximum: 50 }
   validates  :introduction, presence: true, length: { maximum: 200 }
+  validate  :picture_size
+
+  private
+
+    # アップロードされた画像のサイズを制限する
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "：5MBより大きい画像はアップロードできません。")
+      end
+    end
 end
