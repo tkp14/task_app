324077959a3742c5719a3cbd2749cb9b9c50d7c7
tkp14 <tk.091438@gmail.com>
2021-07-06 00:02:09 +0900

    Userモデルのテストにフォロー機能のテスト追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
                                   foreign_key: "follower_id",
                                   dependent: :destroy
   has_many :following, through: :active_relationships,  source: :followed
-  has_many :active_relationships, class_name: "Relationship",
+  has_many :passive_relationships, class_name: "Relationship",
                                   foreign_key: "followed_id",
                                   dependent: :destroy
-  has_many :followers, through: :active_relationships,  source: :follower
+  has_many :followers, through: :passive_relationships,  source: :follower
   attr_accessor :remember_token #仮想の属性を作成
   before_save :downcase_email #セーブする前に小文字にする
   validates :name, presence: true, length: { maximum: 50 }
