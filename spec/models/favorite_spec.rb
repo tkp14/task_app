afaedc6b9faae1eca5cff16986748c2ed70906ba
tkp14 <tk.091438@gmail.com>
2021-07-20 00:17:32 +0900

    Favoritesのモデルテスト作成

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:favorite) { create(:favorite) }
+
+  context "バリデーション" do
+    it "関係性が有効であること" do
+      expect(favorite).to be_valid
+    end
+
+    it "user_idがnilの場合は関係性が無効になること" do
+      favorite.user_id = nil
+      expect(favorite).not_to be_valid
+    end
+
+    it "task_idがnilの場合は関係性が無効になること" do
+      favorite.task_id = nil
+      expect(favorite).not_to be_valid
+    end
+  end
 end
