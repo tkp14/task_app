bb6702f7577d4b02d013fe10430e038e1e0cbdb3
tkp14 <tk.091438@gmail.com>
2021-07-22 00:15:58 +0900

    Favoritesのリクエストテスト修正2

--- spec/requests/favorites_request_spec.rb
+++ spec/requests/favorites_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "お気に入り登録機能", type: :request do
   let(:user) { create(:user) }
   let(:task) { create(:task) }
 
-  context "お気に入り登録処理" do
+  describe "お気に入り登録処理" do
     context "ログインしていない場合" do
       it "お気に入り登録は実行できず、ログインページへリダイレクトすること" do
         expect {
