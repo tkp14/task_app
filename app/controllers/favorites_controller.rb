d822f48b48f651c2d8d06a94959021a8f323a3aa
tkp14 <tk.091438@gmail.com>
2021-07-23 23:53:43 +0900

    Favoritesコントローラのcreate,destroyアクション作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,21 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @task = Task.find(params[:task_id])
+    @user = @task.user
+    current_user.favorite(@task)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    @task = Task.find(params[:task_id])
+    current_user.favorites.find_by(task_id: @task.id).destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
