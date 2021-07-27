389e23eff1c68f33e2356640500832b6dd302e22
tkp14 <tk.091438@gmail.com>
2021-07-27 22:34:51 +0900

    リクエストテスト作成

--- spec/requests/favorites_request_spec.rb
+++ spec/requests/favorites_request_spec.rb
@@ -5,6 +5,36 @@ RSpec.describe "お気に入り登録機能", type: :request do
   let(:task) { create(:task) }
 
   describe "お気に入り登録処理" do
+    context "ログインしている場合" do
+      before do
+        login_for_request(user)
+      end
+
+      it "お気に入り登録できること" do
+        expect {
+          post "/favorites/#{task.id}/create"
+        }.to change(Favorite, :count).by(1)
+      end
+
+      it "Ajaxによるお気に入り登録できること" do
+        expect {
+          post "/favorites/#{task.id}/create", xhr: true
+        }.to change(Favorite, :count).by(1)
+      end
+
+      it "お気に入りの解除ができること" do
+        expect {
+          delete "/favorites/#{task.id}/destroy"
+        }.to change(Favorite, :count).by(-1)
+      end
+
+      it "Ajaxによるお気に入りの解除ができること" do
+        expect {
+          delete "/favorites/#{task.id}/destroy", xhr: true
+        }.to change(Favorite, :count).by(-1)
+      end
+    end
+
     context "ログインしていない場合" do
       it "お気に入り登録は実行できず、ログインページへリダイレクトすること" do
         expect {
