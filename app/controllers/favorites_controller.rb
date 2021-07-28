638293618744076cff73888ccbe478a9e20e2ab3
tkp14 <tk.091438@gmail.com>
2021-07-28 23:08:31 +0900

    Favoritesコントローラにindexアクションを追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
   before_action :logged_in_user
 
+  def index
+    @favorites = current_user.favorite
+  end
+
   def create
     @task = Task.find(params[:task_id])
     @user = @task.user
