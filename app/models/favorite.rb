2b88435f1613889f3e42d803d4e4b9af855da1cf
tkp14 <tk.091438@gmail.com>
2021-07-29 22:59:13 +0900

    Favoriteモデルに降順の指定を追加

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :task
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :task_id, presence: true
 end
