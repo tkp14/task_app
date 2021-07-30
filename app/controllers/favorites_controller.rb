81e2eb6a6f1e08a8135e1d9badcec7038e2a556e
tkp14 <tk.091438@gmail.com>
2021-07-30 23:54:37 +0900

    indexアクション修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @favorites = current_user.favorite
+    @favorites = current_user.favorites
   end
 
   def create
