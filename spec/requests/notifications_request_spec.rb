531227ae78c92c01027cd186044203cb9577c776
tkp14 <tk.091438@gmail.com>
2021-08-21 18:42:11 +0900

    リクエストテスト作成

--- spec/requests/notifications_request_spec.rb
+++ spec/requests/notifications_request_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "Notifications", type: :request do
+RSpec.describe "通知機能", type: :request do
+  let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/notifications/index"
-      expect(response).to have_http_status(:success)
+  describe "通知一覧ページ" do
+    context "ログインしている場合" do
+      before do
+        login_for_request(user)
+      end
+
+      it "通知一覧ページが正しく表示されること" do
+        get notifications_path
+        expect(response).to have_http_status "200"
+        expect(response).to render_template('notifications/index')
+      end
+    end
+
+    context "ログインユーザーではない場合" do
+      it "通知一覧ページが表示されず、ログインページへリダイレクトされること" do
+        get notifications_path
+        expect(response).to have_http_status "302"
+        expect(response).to redirect_to login_path
+      end
     end
   end
-
 end
