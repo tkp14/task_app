c27e40a40dae1b049042262c9feafad4ab281d1b
tkp14 <tk.091438@gmail.com>
2021-08-21 17:05:48 +0900

    ファクトリ作成

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :notification do
-    user_id 1
     task_id 1
     variety 1
     content "MyText"
-    from_user_id 1
+    from_user_id 2
+    association :user
   end
 end
