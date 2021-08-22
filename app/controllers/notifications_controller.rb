18f15641206749392e343999004a13ead836da42
tkp14 <tk.091438@gmail.com>
2021-08-22 11:40:03 +0900

    Notificationsコントローラのindexアクション追記

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,9 @@ class NotificationsController < ApplicationController
   before_action :logged_in_user
 
   def index
+    # current_userに対する通知の集合を取得
+    @notifications = current_user.notifications
+    # 一度indexページを開いたら、ユーザーの「通知フラグ」を削除
+    current_user.update.attribute(:notification, false)
   end
 end
