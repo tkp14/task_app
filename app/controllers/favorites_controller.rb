e63f2f20c468813a0629842ec280aecf96cf5395
tkp14 <tk.091438@gmail.com>
2021-08-22 10:47:18 +0900

    お気に入り登録された時に通知を作成する設定をfavoritesアクションに追記

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,11 +7,18 @@ class FavoritesController < ApplicationController
 
   def create
     @task = Task.find(params[:task_id])
+    @user = @task.user
     #どのtaskにいいねするか
     current_user.favorite(@task)
     respond_to do |format|
       format.html { redirect_to request.referer || root_url }
       format.js
+    end
+    #自分以外のユーザーからお気に入り登録があった時のみ通知を作成
+    if @user != current_user
+      @user.notifications.create(task_id: @task.id, variety: 1,
+                                 from_user_id: current_user.id)# お気に入り登録は通知種別1
+      @user.update_attribute(:notification, true)
     end
   end
 
