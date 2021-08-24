b3f9e69a77a6b8b02d9ae0b620c0aecb1a3e7294
tkp14 <tk.091438@gmail.com>
2021-08-24 23:44:37 +0900

    Listモデル作成

---
+++ db/migrate/20210824144412_create_lists.rb
@@ -0,0 +1,11 @@
+class CreateLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :lists do |t|
+      t.integer :user_id
+      t.integer :task_id
+      t.integer :from_user_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/lists.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :list do
+    user_id 1
+    task_id 1
+    from_user_id 1
+  end
+end
