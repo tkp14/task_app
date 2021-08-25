812ad591a251b587f6091d2c4ce38c3a2c8b5bda
tkp14 <tk.091438@gmail.com>
2021-08-25 23:48:57 +0900

    関連付け、バリデーション追加

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,7 @@
 class List < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :task_id, presence: true
+  validates :from_user_id, presence: true
+  default_scope -> { order(created_at: :desc) }
 end
